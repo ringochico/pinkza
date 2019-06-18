8cf1bb2202396bf7ca25a092787abcc06ac43396
Larry Nevins <ichbinlarry@gmail.com>
2019-06-18 13:01:48 +0700

    added cards to product page

--- pinkza/js/scripts.js
+++ pinkza/js/scripts.js
@@ -19,12 +19,11 @@ setInterval(function()
 {
   $('.preload img').css({'transform':'rotate('+ degree +'deg)'});
   degree++;
-}, 6)
+}, 6);
 
 // function get rid of preload
-window.addEventListener('load', () => {
-  const preloader =  document.querySelector('.preload');
-  preload.classList.add('preload-finish');
-
+window.addEventListener('load', function () {
+  const loader =  document.querySelector('.preload');
+  loader.className += " hidden"; // class "loader" then "hidden"
 });
 
