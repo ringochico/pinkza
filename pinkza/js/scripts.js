d0b575e970d42cbc8d4cde836ddf8c75fee50579
Larry Nevins <ichbinlarry@gmail.com>
2019-06-21 10:13:06 +0700

    added btns to cards

--- pinkza/js/scripts.js
+++ pinkza/js/scripts.js
@@ -30,16 +30,37 @@
 
 
 
-// js to create  a simple map
- var map = new ol.Map({
-        target: 'map',
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.OSM()
-          })
-        ],
-        view: new ol.View({
-          center: ol.proj.fromLonLat([20.41, 7.50]),
-          zoom: 4
-        })
-      });
+// // js to create  a simple map
+//  var map = new ol.Map({
+//         target: 'map',
+//         layers: [
+//           new ol.layer.Tile({
+//             source: new ol.source.OSM()
+//           })
+//         ],
+//         view: new ol.View({
+//           center: ol.proj.fromLonLat([20.41, 7.50]),
+//           zoom: 4
+//         })
+//       });
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
